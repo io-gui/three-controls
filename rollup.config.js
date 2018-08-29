981b479b0ca12a0a5220fb21f1b5eeadfbfb6b0e
Aki Rodic <aleksandar.xyz@gmail.com>
2018-08-29 18:14:00 +0200

    Cleanup and rebuild.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 
-function configureOutput( file ) {
+function output( file ) {
   return {
     format: 'es',
     file: file,
@@ -9,7 +9,7 @@ function configureOutput( file ) {
   }
 }
 
-function externalPaths( list ) {
+function external( list ) {
   const paths = [];
   for (var i = 0; i < list.length; i++) {
     paths.push( path.resolve( list[i] ) )
@@ -20,75 +20,75 @@ function externalPaths( list ) {
 export default [
   {
     input: 'src/Control.js',
-    output: configureOutput( 'build/Control.js'),
-    external: externalPaths( ['../three.js/build/three.module.js'] )
+    output: output( 'build/Control.js'),
+    external: external( ['../three.js/build/three.module.js'] )
   },
   {
     input: 'src/Selection.js',
-    output: configureOutput( 'build/Selection.js'),
-    external: externalPaths( ['../three.js/build/three.module.js'] )
+    output: output( 'build/Selection.js'),
+    external: external( ['../three.js/build/three.module.js'] )
   },
   {
-    input: 'src/ViewportControls/ViewportControls.js',
-    output: configureOutput( 'build/ViewportControls/ViewportControls.js' ),
-    external: externalPaths( [
+    input: 'src/controls/ViewportControls.js',
+    output: output( 'build/controls/ViewportControls.js' ),
+    external: external( [
       './src/Control.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ViewportControls/OrbitControls.js',
-    output: configureOutput( 'build/ViewportControls/OrbitControls.js' ),
-    external: externalPaths( [
-      'src/ViewportControls/ViewportControls.js',
+    input: 'src/controls/OrbitControls.js',
+    output: output( 'build/controls/OrbitControls.js' ),
+    external: external( [
+      'src/controls/ViewportControls.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ViewportControls/SelectionControls.js',
-    output: configureOutput( 'build/ViewportControls/SelectionControls.js' ),
-    external: externalPaths( [
-      'src/ViewportControls/ViewportControls.js',
+    input: 'src/controls/SelectionControls.js',
+    output: output( 'build/controls/SelectionControls.js' ),
+    external: external( [
+      'src/controls/ViewportControls.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ViewportControls/TrackballControls.js',
-    output: configureOutput( 'build/ViewportControls/TrackballControls.js' ),
-    external: externalPaths( [
-      'src/ViewportControls/ViewportControls.js',
+    input: 'src/controls/TrackballControls.js',
+    output: output( 'build/controls/TrackballControls.js' ),
+    external: external( [
+      'src/controls/ViewportControls.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ViewportControls/EditorControls.js',
-    output: configureOutput( 'build/ViewportControls/EditorControls.js' ),
-    external: externalPaths( [
-      'src/ViewportControls/ViewportControls.js',
+    input: 'src/controls/EditorControls.js',
+    output: output( 'build/controls/EditorControls.js' ),
+    external: external( [
+      'src/controls/ViewportControls.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ObjectControls/ObjectControls.js',
-    output: configureOutput( 'build/ObjectControls/ObjectControls.js' ),
-    external: externalPaths( [
-      'src/ObjectControls/ObjectControls.js',
+    input: 'src/controls/ObjectControls.js',
+    output: output( 'build/controls/ObjectControls.js' ),
+    external: external( [
+      'src/controls/ObjectControls.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ObjectControls/DragControls.js',
-    output: configureOutput( 'build/ObjectControls/DragControls.js' ),
-    external: externalPaths( [
-      'src/ObjectControls/ObjectControls.js',
+    input: 'src/controls/DragControls.js',
+    output: output( 'build/controls/DragControls.js' ),
+    external: external( [
+      'src/controls/ObjectControls.js',
       '../three.js/build/three.module.js'
     ] )
   },
   {
-    input: 'src/ObjectControls/TransformControls.js',
-    output: configureOutput( 'build/ObjectControls/TransformControls.js' ),
-    external: externalPaths( [
-      'src/ObjectControls/ObjectControls.js',
+    input: 'src/controls/TransformControls.js',
+    output: output( 'build/controls/TransformControls.js' ),
+    external: external( [
+      'src/controls/ObjectControls.js',
       '../three.js/build/three.module.js'
     ] )
   }
