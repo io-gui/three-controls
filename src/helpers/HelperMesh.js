60ec3d4d8b2e703d4f81a52a838983dfa18eb67f
Aki Rodic <aleksandar.xyz@gmail.com>
2018-10-16 20:19:03 +0200

    Passed geometry props to mesh.

--- src/helpers/HelperMesh.js
+++ src/helpers/HelperMesh.js
@@ -6,13 +6,13 @@ import {Mesh, Vector3} from "../../lib/three.module.js";
 import {HelperMaterial} from "./HelperMaterial.js";
 
 export class HelperMesh extends Mesh {
-	constructor(geometry, props = {}) {
+	constructor(geometry) {
 		super();
+		const props = geometry.props || {};
 		this.geometry = geometry;
-		this.material = new HelperMaterial(props.color, props.opacity || 1);
+		this.material = new HelperMaterial(props);
 		this.scaleTarget = new Vector3(1, 1, 1);
 		this.hidden = false;
-		this.highlight = 0;
-		this.name = props.name;
+		this.highlight = props.highlight || 0;
 	}
 }
